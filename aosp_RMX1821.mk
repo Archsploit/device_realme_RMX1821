b7429e060ddf4630fa74ac5c7f7534bc21dc2ec2
jhenrique09 <jhsv09@gmail.com>
2022-01-19 16:02:04 +0600

    RMX1821: Enable quick tap support

--- aosp_RMX1821.mk
+++ aosp_RMX1821.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common PE stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES:= 720
 
 # Inherit from Realme RMX1821
