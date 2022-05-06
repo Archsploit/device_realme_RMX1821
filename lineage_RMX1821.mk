302d92f1bb38a39359e5ceaa6454c9650c5c84d7
Archsploit <archsploit@protonmail.com>
2022-05-06 06:47:20 +0530

    LineageOS Bringup

--- lineage_RMX1821.mk
+++ lineage_RMX1821.mk
@@ -19,17 +19,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PE stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Pe stuffs
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES:= 720
 
 # Inherit from Realme RMX1821
 $(call inherit-product, device/realme/RMX1821/device.mk)
 
-PRODUCT_NAME := aosp_RMX1821
+PRODUCT_NAME := lineage_RMX1821
 PRODUCT_DEVICE := RMX1821
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := Realme
